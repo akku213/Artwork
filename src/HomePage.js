a47b054d36375b80758ea48c466e67bfea2fd491
akku213 <akankshashrivastav2103@gmail.com>
2024-03-18 21:45:33 +0530

    commit

---
+++ src/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./HomePage.css";
+const HomePage = () => {
+  return (
+    <>
+    <div className='main-1'>
+        <div>
+        <h1>Welcome</h1>
+        </div>
+        <div className='q-1'>
+            YOU'RE WORTHY JUST AS YOU ARE❤️
+        </div> 
+        <div className='card'>
+            <div className="c-1">
+                Card
+            </div>
+            <div className="c-2">
+                Card
+            </div>
+        </div> 
+    </div>
+    </>
+  )
+}
+
+export default HomePage;
